f634bea030ede9ad7b492450e7c6f8fc8b715a38
Miguel Castillo <manchagnu@gmail.com>
2014-02-11 21:32:28 -0500

    Adding repo URL to the copyright notice

--- buildinfo/license.frag
+++ buildinfo/license.frag
@@ -1,5 +1,7 @@
 /**
  * koFactory Copyright (c) 2014 Miguel Castillo.
  * Licensed under MIT
+ *
+ * https://github.com/MiguelCastillo/koFactory
  */
 
--- dist/ko.factory-debug.js
+++ dist/ko.factory-debug.js
@@ -1,6 +1,8 @@
 /**
  * koFactory Copyright (c) 2014 Miguel Castillo.
  * Licensed under MIT
+ *
+ * https://github.com/MiguelCastillo/koFactory
  */
 
 
